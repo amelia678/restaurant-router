ac2e3573d9eda852d893d677b630f6bd36977da4
Amelia Schulz <amelia.schulz@gmail.com>
2018-12-11 14:54:11 -0500

    makes neighborhoods subnav of restaurants

--- src/Restaurants.js
+++ src/Restaurants.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Restaurants = (props) => {
     return (
         <div>
-            <h1>FOOD</h1>
+            <h1>FOOD by neighborhood</h1>
+            
+           
+                <ul>
+                    <li>
+                        <Link to="/restaurants/buckhead">Buckhead</Link>
+                    </li>
+                    <li>
+                        <Link to="/restaurants/decatur">Decatur</Link>
+                    </li>
+                    <li>
+                        <Link to="/restaurants/midtown">Midtown</Link>
+                    </li>
+                </ul>
+            
         </div>
     )
 }
